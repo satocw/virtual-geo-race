0cfc318594c87d912cc745db6f5c2900d61efe13
NISHI Satoshi <nishi@flexsche.com>
2018-08-21 19:04:18 +0900

    add material and leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'virtual-geo-race';
+
+  options = {
+    layers: [
+      // tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...'
+      })
+    ],
+    zoom: 15,
+    // center: latLng(46.879966, -121.726909)
+    center: latLng(35.3622222, 138.7313889)
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+const MaterialModule = [MatButtonModule, MatCheckboxModule];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LeafletModule.forRoot(),
+    ...MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
