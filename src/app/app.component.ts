490c463d3fd059fe60bb9553c4e6a813ee54b56f
NISHI Satoshi <nishi@flexsche.com>
2018-08-22 18:48:05 +0900

    adding marker test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { Component, OnInit } from '@angular/core';
+import { tileLayer, latLng, polyline, Layer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'virtual-geo-race';
 
   options = {
@@ -19,6 +19,51 @@ export class AppComponent {
     ],
     zoom: 15,
     // center: latLng(46.879966, -121.726909)
-    center: latLng(35.3622222, 138.7313889)
+    // center: latLng(35.3622222, 138.7313889)  // 剣が峰
+    center: latLng(35.55100763216615, 139.67241673730314)
   };
+
+  markers: Layer[] = [];
+
+  ngOnInit() {
+    this.addPolyline();
+  }
+
+  addPolyline() {
+    const newPolyline = polyline([
+      latLng(35.55100763216615, 139.67241673730314),
+      latLng(35.551018277183175, 139.6741683036089),
+      latLng(35.551711628213525, 139.68011434189975)
+    ]);
+
+    this.markers.push(newPolyline);
+
+    const newMarker = marker(latLng(35.551018277183175, 139.6741683036089), {
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    });
+
+    this.markers.push(newMarker);
+
+    setTimeout(() => {
+      const newMarker2 = marker(
+        latLng(35.551711628213525, 139.68011434189975),
+        {
+          icon: icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 41],
+            iconUrl: 'assets/marker-icon.png',
+            shadowUrl: 'assets/marker-shadow.png'
+          })
+        }
+      );
+
+      this.markers.pop();
+      this.markers.push(newMarker2);
+    }, 2000);
+  }
 }
