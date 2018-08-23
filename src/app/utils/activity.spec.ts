85b4e54f0ed5af0ccf1ece339f097665fc12bf98
NISHI Satoshi <nishi@flexsche.com>
2018-08-23 15:57:06 +0900

    adding indexers

--- src/app/utils/activity.spec.ts
+++ src/app/utils/activity.spec.ts
@@ -1,5 +1,11 @@
 import xml2js from './xml2js';
-import { pluckTrackpoints } from './activity';
+import {
+  getActivity,
+  getLaps,
+  getTrackpointsForLap,
+  getTrackpoints,
+  getLatLngForTrackpoint
+} from './activity';
 import { data as _data } from '../../../testing/mockdata-tcx';
 
 describe('utils::activity', () => {
@@ -7,12 +13,38 @@ describe('utils::activity', () => {
   beforeAll(async () => {
     data = await xml2js(_data);
   });
-  it('1', () => {
-    console.log(data['TrainingCenterDatabase']['Activities'][0]);
+  it('data', () => {
     expect(data).not.toBeNull();
   });
 
-  // it('2', () => {
-  //     expect(data['TrainigData'])
-  // });
+  it('getActivity', () => {
+    // console.log(getActivity(data));
+    expect(getActivity(data)).toBeDefined();
+    expect(getActivity(data)['Id']).toBeDefined();
+  });
+
+  it('getLaps', () => {
+    // console.log(getLaps(data));
+    expect(Array.isArray(getLaps(data))).toBeTruthy();
+    expect(getLaps(data).length).toEqual(3);
+  });
+
+  it('getTrackpointsForLap', () => {
+    const _1stlap = getLaps(data)[0];
+    // console.log(_1stlap);
+    expect(Array.isArray(getTrackpointsForLap(_1stlap))).toBeTruthy();
+    expect(getTrackpointsForLap(_1stlap).length).toEqual(3);
+  });
+
+  it('getTrackpoints', () => {
+    expect(getTrackpoints(data).length).toEqual(9);
+  });
+
+  it('getLatLngForTrackpoint', () => {
+    const trackpoints = getTrackpoints(data);
+
+    console.log(trackpoints[0]);
+
+    console.log(getLatLngForTrackpoint(trackpoints[0]));
+  });
 });
--- src/app/utils/activity.ts
+++ src/app/utils/activity.ts
@@ -1,1 +1,36 @@
-export function pluckTrackpoints() {}
+import { access } from 'fs';
+
+export function getActivity(data: {
+  [key: string]: any;
+}): { [key: string]: any } {
+  return data['TrainingCenterDatabase']['Activities'][0]['Activity'][0];
+}
+
+export function getLaps(data: {
+  [key: string]: any;
+}): { [key: string]: any }[] {
+  return getActivity(data)['Lap'];
+}
+
+export function getTrackpointsForLap(lap: {
+  [key: string]: any;
+}): { [key: string]: any }[] {
+  return lap['Track'][0]['Trackpoint'];
+}
+
+export function getTrackpoints(data: {
+  [key: string]: any;
+}): { [key: string]: any }[] {
+  return getLaps(data)
+    .map(lap => getTrackpointsForLap(lap))
+    .reduce((acc, cur) => [...acc, ...cur], []);
+}
+
+export function getLatLngForTrackpoint(trackpoint: { [key: string]: any }) {
+  return [
+    ...trackpoint['Position'][0]['LatitudeDegrees'],
+    ...trackpoint['Position'][0]['LongitudeDegrees']
+  ].map(stringToNumber);
+}
+
+const stringToNumber = (value: string) => +value;
